3b9e9800bb32578a762aa935047607c0e58210c2
LWL220184016 <145105128+LWL220184016@users.noreply.github.com>
2025-07-21 06:57:40 +0000

    remove useless debug msg

--- data/collators.py
+++ data/collators.py
@@ -26,11 +26,7 @@ class AlignmentCollator(object):
             max_length=self.text_max_length,
             return_tensors="pt"
         )
-        print(f"Debug(collators): Encoded texts: {encoded_texts}")  # Debugging line to check encoded texts
-        print(f"Debug(collators): Encoded texts type: {type(encoded_texts)}")  # Debugging line to check encoded texts
-        print(f"Debug(collators): Encoded texts type: {tyinputpe(encoded_texts["input_ids"])}")  # Debugging line to check encoded texts
-        print(f"Debug(collators): Encoded texts type: {type(encoded_texts["attention_mask"])}")  # Debugging line to check encoded texts
-
+        
         # 返回對齊任務需要的數據
         return {
             "audio": audios,
